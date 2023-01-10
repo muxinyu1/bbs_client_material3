eb2fb63fb6ba1ee084ae4e890b7c3e7872a28a2b
mxy <13614169547@163.com>
2023-01-10 20:13:41 +0800

    修正了评论后不能正确刷新的Bug

--- app/src/main/java/com/mxy/bbs_client/program/state/HomeScreenState.kt
+++ app/src/main/java/com/mxy/bbs_client/program/state/HomeScreenState.kt
@@ -1,7 +1,7 @@
 package com.mxy.bbs_client.program.state
 
 data class HomeScreenState(
-    val postList: List<PostState>,
+    var postList: List<PostState>,
     val openedPost: String? = null,
     val placeHolder: Int = 0,
     val postState: PostState  = DefaultPostState
